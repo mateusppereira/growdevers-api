ac7ee066bc02f981994c96341e069340f883caa9
Mateus Pereira <mateuspereira97@icloud.com>
2023-04-03 18:56:09 -0300

    remove dist

--- dist/controllers/updateGrowdever.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.updateGrowdeverController = void 0;
-const updateGrowdeverController = (req, res) => {
-    console.log('update growdever');
-};
-exports.updateGrowdeverController = updateGrowdeverController;
-//# sourceMappingURL=updateGrowdever.js.map
--- dist/middlewares/authMiddleware copy.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=authMiddleware%20copy.js.map
